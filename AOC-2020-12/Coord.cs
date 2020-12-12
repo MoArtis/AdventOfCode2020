4f33de248321daf1206b834f00949a7e8eb35c74
Mathieu Philippe <mathieuphilippe.dev@gmail.com>
2020-12-12 15:20:31 +0800

    Add day 12

---
+++ AOC-2020-12/Coord.cs
@@ -0,0 +1,98 @@
+﻿public struct Coord
+{
+    public override string ToString()
+    {
+        return $"x: {x} | y: {y}";
+    }
+
+    public static Coord operator -(Coord v) => new Coord(-v.x, -v.y);
+
+    public static Coord operator +(Coord a, Coord b) => new Coord(a.x + b.x, a.y + b.y);
+
+    public static Coord operator -(Coord a, Coord b) => new Coord(a.x - b.x, a.y - b.y);
+
+    public static Coord operator *(Coord a, Coord b) => new Coord(a.x * b.x, a.y * b.y);
+
+    public static Coord operator *(int a, Coord b) => new Coord(a * b.x, a * b.y);
+
+    public static Coord operator *(Coord a, int b) => new Coord(a.x * b, a.y * b);
+
+    public static Coord operator /(Coord a, int b) => new Coord(a.x / b, a.y / b);
+
+    public static bool operator ==(Coord lhs, Coord rhs) => lhs.x == rhs.x && lhs.y == rhs.y;
+
+    public static bool operator !=(Coord lhs, Coord rhs) => !(lhs == rhs);
+
+    public static implicit operator Coord((int, int) tuple)
+    {
+        var (x, y) = tuple;
+        return new Coord(x, y);
+    }
+    
+    public bool Equals(Coord other)
+    {
+        return x == other.x && y == other.y;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Coord other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return (x * 397) ^ y;
+        }
+    }
+
+    public static Coord Zero { get; } = new Coord(0, 0);
+
+    public Coord(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x;
+    public int y;
+    
+    public void Rotate(int rotation)
+    {
+        switch (rotation)
+        {
+            case 1:
+                Rotate90();
+                break;
+            
+            case 2:
+                Rotate180();
+                break;
+            
+            case 3:
+                Rotate270();
+                break;
+        }
+    }
+    
+    public void Rotate90()
+    {
+        var px = x;
+        x = -y;
+        y = px;
+    }
+    
+    public void Rotate180()
+    {
+        x = -x;
+        y = -y;
+    }
+    
+    public void Rotate270()
+    {
+        var px = x;
+        x = y;
+        y = -px;
+    }
+}
---
+++ AOC-2020-12/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AOC_2020_12
+{
+    class Program
+    {
+        private Coord[] _directions = {(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)};
+
+        private Dictionary<char, int> _cardinalToDirIndex = new Dictionary<char, int>
+            {{'E', 0}, {'S', 2}, {'W', 4}, {'N', 6}};
+
+        private static int Repeat(int t, int length)
+        {
+            t = (int) (t - MathF.Floor(t / (float) length) * length);
+            return Math.Clamp(t, 0, length);
+        }
+
+        private static void Main()
+        {
+            var program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+            var inputPath = "./input.txt";
+            // var inputPath = "./testInput.txt";
+
+            var route = ParseRoute(inputPath);
+
+            //Part 1
+            var manDistance = ComputeRouteManhattanDistance(route);
+            Console.WriteLine($"The manhattan distance for part 1 is : {manDistance}");
+
+            //Part 2
+            manDistance = ComputeRouteManhattanDistancePart2(route, (10, 1));
+            Console.WriteLine($"The manhattan distance for part 2 is : {manDistance}");
+        }
+
+        private int ComputeRouteManhattanDistancePart2((char, int)[] route, Coord startWpPosition)
+        {
+            var position = new Coord(0, 0);
+            var wpPosition = startWpPosition;
+
+            foreach (var (instruction, value) in route)
+            {
+                switch (instruction)
+                {
+                    case 'F':
+                        position += wpPosition * value;
+                        break;
+
+                    case 'L':
+                        wpPosition.Rotate(Repeat(value / 90, 4));
+                        break;
+
+                    case 'R':
+                        wpPosition.Rotate(Repeat(-value / 90, 4));
+                        break;
+
+                    default:
+                        var dirIndex = _cardinalToDirIndex[instruction];
+                        wpPosition += _directions[dirIndex] * value;
+                        break;
+                }
+
+                // Console.WriteLine($"{instruction}{value} => Pos: ({position.x}|{position.y}) - WpPos: ({wpPosition.x}|{wpPosition.y})");
+            }
+
+            return Math.Abs(position.x) + Math.Abs(position.y);
+        }
+
+        private int ComputeRouteManhattanDistance((char, int)[] route)
+        {
+            var position = new Coord(0, 0);
+            var direction = 0;
+
+            foreach (var (instruction, value) in route)
+            {
+                switch (instruction)
+                {
+                    case 'F':
+                        position += _directions[direction] * value;
+                        break;
+
+                    case 'L':
+                        direction = Repeat(direction - value / 45, _directions.Length);
+                        break;
+
+                    case 'R':
+                        direction = Repeat(direction + value / 45, _directions.Length);
+                        break;
+
+                    default:
+                        var dirIndex = _cardinalToDirIndex[instruction];
+                        position += _directions[dirIndex] * value;
+                        break;
+                }
+
+                // Console.WriteLine($"{instruction}{value} => Pos: ({position.x}|{position.y}) - Dir: {direction}");
+            }
+
+            return Math.Abs(position.x) + Math.Abs(position.y);
+        }
+
+        private (char, int)[] ParseRoute(string inputPath)
+        {
+            if (File.Exists(inputPath) == false)
+                return null;
+
+            var lines = File.ReadAllLines(inputPath);
+
+            var route = new (char, int)[lines.Length];
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                route[i] = (line[0], int.Parse(line.Remove(0, 1)));
+            }
+
+            return route;
+        }
+    }
+}
